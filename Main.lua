e50397ad8da0748d9372dd5dc37940f9bf7168ab
CrazyHub <78809386+CrazyHub123@users.noreply.github.com>
2025-01-23 20:45:21 +0200

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,6 +1,7 @@
 _G.Scripts = {
     [4069560710] = "https://raw.githubusercontent.com/CrazyHub123/NexusHubRevival/refs/heads/main/OneFruitSimulator.lua",
     [6594435384] = "https://raw.githubusercontent.com/CrazyHub123/NexusHubRevival/refs/heads/main/SpongebobTowerDefense.lua",
+    [5682590751] = "https://raw.githubusercontent.com/CrazyHub123/NexusHubRevival/refs/heads/main/Lootify.lua",
 }
 
 if not _G.Override then
