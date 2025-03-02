7ffd6ec4a699574ccb969765a0a418f44f498074
CrazyHub <78809386+CrazyHub123@users.noreply.github.com>
2025-03-03 01:58:50 +0200

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -4,6 +4,7 @@ _G.Scripts = {
     [5682590751] = "https://raw.githubusercontent.com/CrazyHub123/NexusHubRevival/refs/heads/main/Lootify.lua",
     [6471449680] = "https://raw.githubusercontent.com/CrazyHub123/NexusHubRevival/refs/heads/main/VersePiece.lua",
     [3110388936] = "https://raw.githubusercontent.com/CrazyHub123/NexusHubRevival/refs/heads/main/NinjaTime.lua",
+    [6325068386] = "https://raw.githubusercontent.com/CrazyHub123/NexusHubRevival/refs/heads/main/BlueLockRivals.lua",
 }
 
 if not _G.Override then
