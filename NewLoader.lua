4202747f15d0eb6e4535d4ac6360b50751ec770b
CrazyHub <78809386+CrazyHub123@users.noreply.github.com>
2025-03-27 22:47:39 +0200

    Update NewLoader.lua

--- NewLoader.lua
+++ NewLoader.lua
@@ -2,7 +2,8 @@ _G.Scripts = {
     [7074860883] = "187ba48289a2b0ea30d7a14ac3b1adb6",
     [3110388936] = "b7eaa8d73e8f9c65160a71b859b750f5",
     [6325068386] = "fe1f887df195d6304f30fbf3eb2f0e48",
-    [6471449680] = "e2381843cef1eecf8bd0958090be3109"
+    [6471449680] = "e2381843cef1eecf8bd0958090be3109",
+    [4069560710] = "02d46e8de0dbbb9df0c1bdf7c306e83c"
 }
 
 if not _G.Override then
